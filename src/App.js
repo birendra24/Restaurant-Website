843c43e22f0446a55f38e36c923cea7cf01c99a5
biru <birendrakshetri90801@gmail.com>
2023-04-28 10:45:25 +0545

    second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Resturant from "./components/Resturant";
+
+const App = () => {
+  return (
+    <>
+      <Resturant />
+    </>
+  );
+};
+
+export default App;
